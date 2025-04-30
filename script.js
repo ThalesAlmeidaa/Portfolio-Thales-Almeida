b68fc5d5fe867218744475be1901085e9490f8b7
ThalesAlmeidaa <thalestr36@gmail.com>
2025-04-29 21:00:43 -0300

    Inclusão de responsividade para celulares

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
+
+  function moverImagemResponsivo() {
+    const largura = window.innerWidth;
+    const imagem = document.getElementById('minhaImagem');
+    const divImg = document.getElementById('img');
+    const textoSobre = document.getElementById('textoSobre');
+    const paragrafo = textoSobre.querySelector('p');
+
+    if (!imagem || !divImg || !textoSobre || !paragrafo) return;
+
+    if (largura <= 428) {
+      
+      if (imagem.parentElement !== textoSobre) {
+        textoSobre.insertBefore(imagem, paragrafo);
+      }
+    } else {
+      
+      if (imagem.parentElement !== divImg) {
+        divImg.appendChild(imagem);
+      }
+    }
+  }
+
+  window.addEventListener('load', moverImagemResponsivo);
+  window.addEventListener('resize', moverImagemResponsivo);
